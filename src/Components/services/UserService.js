bbacb6caf83531f2aaa8f8a04ed4ebc18743e2c0
Галихан Хамитов <galikhan.khamitov@zee.kz>
2024-01-12 16:54:49 +0600

    use deafult api url

--- src/Components/services/UserService.js
+++ src/Components/services/UserService.js
@@ -3,8 +3,9 @@ import { URL } from '../Common/ddata';
 const UserService = {
 
     login: function (formData) {
-        // return fetch(URL + "/login", {
-        return fetch("http://localhost:8180/login", {
+        let url = URL.replace('/api', '');
+        console.log('url', url);
+        return fetch(url + '/login', {
             method: "POST",
             headers: { "Content-type": "application/json" },
             body: JSON.stringify(formData),
