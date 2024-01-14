3c47004f8fa5a02ea7994fb442f41d6491677acd
Shyngis <shyngyserlan0@gmail.com>
2024-01-14 20:10:04 +0600

    slider

--- src/Components/Catalog/Catalog.jsx
+++ src/Components/Catalog/Catalog.jsx
@@ -5,6 +5,10 @@ import { Button, Card } from "react-bootstrap";
 import CategoryService from "../services/CategoryService";
 import "./Catalog.css";
 import { CatalogFilter } from "./CatalogFilter";
+import carou from "../../Assets/carou.jpg";
+import cow from "../../Assets/cow.png";
+import lake from "../../Assets/lake.jpg";
+import { MDBCarousel, MDBCarouselItem } from "mdb-react-ui-kit";
 
 export const Catalog = () => {
   const [mainCategories, setMainCategories] = useState([]);
@@ -39,6 +43,17 @@ export const Catalog = () => {
 
   return (
     <>
+      <MDBCarousel fade interval={5000} className="slider_wrap">
+        <MDBCarouselItem itemId={1} className="slider">
+          <img src={carou} className="d-block w-100" alt="..." />
+        </MDBCarouselItem>
+        <MDBCarouselItem itemId={2} className="slider">
+          <img src={cow} className="d-block w-100" alt="..." />
+        </MDBCarouselItem>
+        <MDBCarouselItem itemId={3} className="slider">
+          <img src={lake} className="d-block w-100" alt="..." />
+        </MDBCarouselItem>
+      </MDBCarousel>
       <div>
         <div className="row">
           <div className="col-md-12">
